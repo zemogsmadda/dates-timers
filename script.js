d67d3b141d3128ea34ca9c252c1074f8541e5b05
Paul Gonzales <paulgonzales@Pauls-Mac-mini.local>
2020-12-16 23:34:50 -0800

    Math

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,101 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+//
+
+/////////////////////////////////////////////////
+// CONVERTING AND CHECKING NUMBERS
+
+/*
+CONVERSION
+PARSING
+PARSE FLOAT
+ISNAN
+ISFINITE
+*/ 
+
+/*
+//CONVERSION
+console.log(Number(`23`));
+console.log(+`23`); //TYPE COERCION WILL TURN IT INTO A NUMBER
+
+//PARSING
+//BASE 10 = 0 - 9
+//BINARY BASE 2 = 0 & 1
+console.log(Number.parseInt(`30px`, 10)); //30 STRING NEEDS TO START WITH A NUMBER
+console.log(Number.parseInt(`e23`, 10)); //NAN
+
+//PARSE FLOAT
+console.log(Number.parseFloat(`2.5rem`)); //2.5
+console.log(Number.parseInt(`2.5rem`)); //2
+
+//ISNAN
+console.log(Number.isNaN(20)); //F
+console.log(Number.isNaN(`20`)); //F
+console.log(Number.isNaN(+`20x`)); //T
+console.log(Number.isNaN(23 / 0)); //F
+
+//IS FINITE
+//THE BEST WAY TO CHECK IF A VALUE IS A NUMBER
+console.log(Number.isFinite(20)); //T
+console.log(Number.isFinite(`20`)); //F
+console.log(Number.isFinite(+`20X`)); //F
+console.log(Number.isFinite(20 / 0)); //F
+*/
+
+/////////////////////////////////////////////////
+// MATH AND ROUNDING
+
+/* 
+SQUARE AND CUBIC ROOT
+MIN MAX RANDOM VALUES
+ROUNDING INTEGERS
+MATH CONSTANTS
+*/
+
+/*
+console.log(Math.sqrt(25)); //5
+console.log(25 ** (1 / 2)); //5
+console.log(8 ** (1 / 3)); //2 CUBIC ROOT
+
+console.log(Math.max(5, 18, 23, 11, 2)); //MAX VALUE IS RETURNED
+console.log(Math.max(5, 18, `23`, 11, 2)); //23
+console.log(Math.max(5, 18, `23px`, 11, 2)); //NAN
+
+console.log(Math.min(5, 18, 23, 11, 2)); //MIN VALUE IS RETURNED
+
+console.log(Math.PI); //MATH CONSTANT 
+console.log(Math.PI * Number.parseFloat(`10px`) ** 2); //HOW TO CALCULATE THE AREA OF A CIRCLE WITH
+                                                        //THIS RADIUS
+
+console.log(Math.random()); //RANDOM NUMBER BETWEEN 0 AND 1
+console.log(Math.trunc(Math.random() * 6) + 1);//RANDOM NUMBER BETWEEN 1 AND 6
+
+//RANDOMIZE A NUMBER BETWEEN A MINIMUM AND MAXIMUM NUMBER
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+const randomInteger = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+};
+
+console.log(randomInteger(1, 3));
+console.log(randomInt(1, 3));              
+
+//ROUNDING INTEGERS
+//FLOOR AND TRUNC WORK THE SAME ONLY WITH + NUMBER
+
+console.log(Math.trunc(23.8)); //23 - WILL REMOVE DECIMAL
+console.log(Math.round(23.8)); //24 - ROUND TO THE NEAREST INTEGER
+console.log(Math.ceil(23.8)); //24 - ROUND UP
+console.log(Math.floor(23.8)); // 23 - ROUND DOWN
+
+console.log(Math.trunc(-23.8)); // -23
+console.log(Math.floor(-23.8)); // -24
+
+//ROUNDING DECIMALS
+// WILL RETURN STRING
+console.log((2.7).toFixed(0)); //3
+console.log((2.7).toFixed(3)); //2.700
+console.log((2.345).toFixed(2)); //2.35
+console.log(+(2.345).toFixed(3)); //CONVERT TO A NUMBER
+*/
