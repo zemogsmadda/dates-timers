31352a394602e9ac0f018189f4a6d006466ded75
Paul Gonzales <paulgonzales@Pauls-Mac-mini.local>
2020-12-18 03:37:58 -0800

    operations with dates

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const account1 = {
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
     '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2020-12-12T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -36,14 +36,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
+    '2019-12-18T13:15:33.035Z',
     '2019-11-30T09:48:16.867Z',
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
     '2020-04-10T14:43:26.374Z',
     '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2020-12-17T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,6 +81,27 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function(date) {
+
+  const daysPassed = function(date1, date2) {
+    const translateDay = (1000 * 60 * 60 * 24);
+    return Math.round(Math.abs(date2 - date1) / translateDay);
+  };
+
+  const calcDaysPassed = daysPassed(new Date(), date);
+  console.log(calcDaysPassed);
+  
+  if (calcDaysPassed === 0) return `Today`;
+  if (calcDaysPassed === 1) return `Yesterday`;
+  if (calcDaysPassed <= 7) return `${calcDaysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+  }
+}
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -90,14 +111,8 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]); //WILL CALL THE SECOND ARRAY USING THE INDEX
-    
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
+    const displayDate = formatMovementDate(date);
 
-
-    const displayDate = `${month}/${day}/${year}`;
-    
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
@@ -480,3 +495,17 @@ future.setFullYear(2040); //HOW TO CHANGE THE SET DATE
 console.log(future); 
 */
 
+/////////////////////////////////////////////////
+//OPERATIONS WITH DATES 
+/*
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const daysPassed = function(date1, date2) {
+  const translateDay = (1000 * 60 * 60 * 24);
+  return Math.abs(date2 - date1) / translateDay;
+};
+
+const days1 = daysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
+console.log(days1);
+*/
